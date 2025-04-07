25f9645bc081c70bf4adf9c287cf92e7a710a74f
embolaweb3 <163683760+embolaweb3@users.noreply.github.com>
2025-04-07 18:17:11 +0000

    push from local repo

--- living-nfts/app/lib/zora.ts
+++ living-nfts/app/lib/zora.ts
@@ -1,4 +1,4 @@
-import { create1155 } from "@zoralabs/protocol-sdk";
+import { create1155,getToken} from "@zoralabs/protocol-sdk";
 import { publicClient, getWalletClient,chain } from "./config";
 
 interface MintParams {
@@ -6,6 +6,8 @@ interface MintParams {
   contractURI: string;
   tokenURI: string;
 }
+
+
 
 export const mintLivingNFT = async (params: MintParams) => {
   const walletClient = await getWalletClient();
@@ -29,4 +31,42 @@ export const mintLivingNFT = async (params: MintParams) => {
     chain: chain, 
   });
   return { hash, contractAddress };
+};
+
+export const getCurrentTokenURI = async (
+  tokenContract: `0x${string}`,
+  tokenId: bigint
+): Promise<string> => {
+  const { token } = await getToken({
+    publicClient,
+    tokenContract,
+    tokenId, 
+    mintType: "1155"
+  });
+
+  if (!token?.tokenURI) throw new Error("Token URI not found");
+  return token.tokenURI;
+};
+
+
+export const prepareEvolution = async (
+  tokenContract: `0x${string}`,
+  tokenId: bigint,
+  minterAccount: `0x${string}`
+) => {
+  // 1. Get token and prepareMint function
+  const { prepareMint } = await getToken({
+    tokenContract,
+    mintType: "1155",
+    tokenId,
+    publicClient
+  });
+
+  if (!prepareMint) throw new Error("Minting not available");
+
+  // 2. Prepare standard mint 
+  return prepareMint({
+    minterAccount,
+    quantityToMint: BigInt(1)
+  });
 };
